044a678a88b49c765c6f0c537ff5b7dbf1cc7df4
bruce larco <br.larco@duocuc.cl>
2024-10-04 00:00:39 -0300

    cancel alert

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionManager } from 'src/managers/SessionManager';
+import { StorageService } from 'src/managers/StorageService';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(
+    private router: Router, 
+    private sessionManager: SessionManager,
+    private storageService: StorageService
+  ) { }
+
+    email: string = '';
+    user: string = '';
+    password: string = '';
+
+  ngOnInit() { }
+
+  async onLoginButtonPressed() {
+    if (this.sessionManager.performLogin(this.user, this.password)) {
+      await this.storageService.set('userEmail', this.email)
+      await this.storageService.set('isSessionActive', true)
+      this.router.navigate(['/home'], { queryParams: { email: this.email } });
+    } else {
+      this.user = '';
+      this.password = '';
+      alert('Las credenciales ingresadas son inválidas.');
+    }
+  }
+
+  onRegisterButtonPressed() {
+    this.router.navigate(['/register']);
+  }
+
+}
